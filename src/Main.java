cf93df87c91bfd3566fc8d7cf783a165d6716c1c
Alexandr <mr-hornet@mail.ru>
2023-03-28 22:57:05 +1000

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        System.out.println();
+        task2();
+        System.out.println();
+        task3();
+        System.out.println();
+    }
+
+    private static void task3() {
+        System.out.println("Задание 3");
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+
+    private static void task2() {
+        System.out.println("Задание 2");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        fullName = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullName);
+
+    }
+
+    private static void task1() {
+        System.out.println("Задание 1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
